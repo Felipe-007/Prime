3c503b22ea1c403ad9f2ae550a77179f7184fa13
Felipe <felipe.presidencia@gmail.com>
2022-08-04 14:57:53 -0300

    C3 - Listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";  // useState pega os filmes da api, useEffect armazena os filmes
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import './styles.css';
 
 export default function Home() {
 
@@ -14,13 +16,26 @@ export default function Home() {
           page: 1,
         }
       })
-      console.log(response.data.results);
+      setFilmes(response.data.results.slice(0, 10))
     }
     loadFilmes();
   }, [])
 
 
   return (
-    <h1>Bem vindo a Home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {  //percorre todo o array de filme
+          return(
+            //encontra o filme pelo id
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+    </div>
   );
 }
