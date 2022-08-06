2e735e8872ee660e21a0e117910e9c2c9c0b8677
Felipe <felipe.presidencia@gmail.com>
2022-08-06 11:27:32 -0300

    C8 - Salvando filme no localStorage

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -36,6 +36,23 @@ export default function Filme() {
     }
   }, [navigate, id]);
 
+  function salvarFilme(){
+    const minhaLista = localStorage.getItem("@primeflix");
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id )
+  
+    if(hasFilme){
+      alert("ESSE FILME JÁ ESTA NA LISTA");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    alert("FILME SALVO COM SUCESSO")
+  }
+
   if (loading) {
     return (
       <div className="filme-info">
@@ -53,9 +70,9 @@ export default function Filme() {
       <strong>Avaliação: {filme.vote_avarage} / 10</strong>
 
       <div className="area-buttons">
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
-          <a target="_blank" rel="noreferrer" href={`https://www.youtube.com/results?search_query=${filme.title} Trailer`}>
+          <a target="blank" rel="external" href={`https://www.youtube.com/results?search_query=${filme.title} Trailer`}>
             Trailer
           </a>
         </button>
