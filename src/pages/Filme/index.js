da2cea46fa2b6a05058a5f25b99f27d2c1b37941
Felipe <felipe.presidencia@gmail.com>
2022-08-06 10:56:54 -0300

    C7 - Ação trailer filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,29 +1,32 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";  //useNavigate = apontará para a página de erro
 import api from "../../services/api";
 import './filme-info.css';
 
 export default function Filme() {
 
-  const {id} = useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();  //apontará para a página de erro
   const [filme, setFilme] = useState();
   const [loading, setLoading] = useState(true);
 
-  useEffect(() =>{
-    async function loadFilme(){
-      await api.get(`/movie/${id}`,{  // aqui fará a requisição para a api, conexão
-        params:{
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {  // aqui fará a requisição para a api, conexão
+        params: {
           api_key: "a4267de68eb55817f90b4867e49b66de",
           language: "pt-BR",
         }
       })
-      .then((response)=>{  //cairá aqui caso de certo
-        setFilme(response.data);  // envia os dados da api para setFilme
-        setLoading(false); //ira desmontar no final para nao carregar em loop
-      })
-      .catch(() =>{
-        console.log("Filme não encontrado");
-      })
+        .then((response) => {  //cairá aqui caso de certo
+          setFilme(response.data);  // envia os dados da api para setFilme
+          setLoading(false); //ira desmontar no final para nao carregar em loop
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+          navigate("*", { replace: true });  //apontará para a página de erro
+          return;
+        })
     }
 
     loadFilme();
@@ -31,28 +34,28 @@ export default function Filme() {
     return () => {
       console.log("Componente desmontado")
     }
-  }, []);
+  }, [navigate, id]);
 
-  if(loading){
-    return(
+  if (loading) {
+    return (
       <div className="filme-info">
         <h1>Carregando detalhes</h1>
       </div>
     )
   }
 
-  return(
+  return (
     <div className="filme-info">
       <h1>{filme.title}</h1>
       <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
       <strong>Avaliação: {filme.vote_avarage} / 10</strong>
-    
+
       <div className="area-buttons">
         <button>Salvar</button>
         <button>
-          <a href="#">
+          <a target="_blank" rel="noreferrer" href={`https://www.youtube.com/results?search_query=${filme.title} Trailer`}>
             Trailer
           </a>
         </button>
